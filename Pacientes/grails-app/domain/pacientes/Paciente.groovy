4e33ec4aa5ad3a9e998f15eb0425bc696a678eae
RuthCruz20 <shuth.1417@gmail.com>
2018-10-08 20:24:41 -0300

    pacientesCorregido

--- Pacientes/grails-app/domain/pacientes/Paciente.groovy
+++ Pacientes/grails-app/domain/pacientes/Paciente.groovy
@@ -1,15 +1,16 @@
 package pacientes
 
 class Paciente {
-    BigInteger nroDocumento
-    String apellido
-    String sexo
-    Date fechaNacimiento
+  BigInteger nroDocumento
+  String apellido
+  String sexo
+  Date fechaNacimiento
 
     static constraints = {
-        nroDocumento (blank: false, unique: true)
-        apellido (blank: false, maxSize:100)
-        sexo (blank: false, inList:['F','M'])
-        fechaNacimiento (blank: false)
+      nroDocumento(blank:false, unique:true)
+      apellido(blank: false, maxSize:100)
+      sexo (blank: false, inList:['F','M'])
+      fechaNacimiento (blank:false)
     }
+
 }
--- Pacientes/grails-app/init/pacientes/BootStrap.groovy
+++ Pacientes/grails-app/init/pacientes/BootStrap.groovy
@@ -1,8 +1,24 @@
 package pacientes
+import pacientes.Paciente
+import grails.validation.ValidationException
 
 class BootStrap {
 
     def init = { servletContext ->
+
+    def paciente1 = new Paciente(fechaNacimiento:Date.parse('yyyy-MM-dd','2015-10-20'),nroDocumento:39805640,apellido:'MIRANDA',sexo:'M')
+    if(!paciente1.save(flush:true)){
+      paciente1.errors.allErrors.each{
+        println it
+      }
+    }
+
+    def fechaD = Date.parse('yyyy-MM-dd','2014-09-01')
+    def fechaH = Date.parse('yyyy-MM-dd','2016-08-30')s
+    def listaPaciente= Paciente.findAll("from Paciente as p where p.fechaNacimiento >= ? and p.fechaNacimiento<=?",[fechaD,fechaH])
+    priintln listaPaciente
+
+
     }
     def destroy = {
     }
