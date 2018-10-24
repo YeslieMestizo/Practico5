fdf9281b4b6969d6c297816cdcb15508c615559a
YeslieMestizo <yeslie.mestizo@gmail.com>
2018-10-24 20:54:17 -0300

    Validaciones

--- grails-app/domain/pacientes/Paciente.groovy
+++ grails-app/domain/pacientes/Paciente.groovy
@@ -16,10 +16,10 @@ class Paciente {
       apellido(blank: false, maxSize:100)
       nombre(blank: false, maxSize:100)
       sexo (blank: false, inList:['F','M'])
-      //fechaNacimiento (blank:false,validator: (it.year >2018))
-      //fechaNacimiento (blank:false,validator: { if ( it.year >2018) return ['anioIncorrecto'] } )
-      fechaNacimiento (blank:false)
-      telefono(blank: false, matches:"[0-9]{3}-[1-9]{6}")
+      fechaNacimiento validator: {
+        Date fechaActual = new Date();
+          if(it>fechaActual) return ['anioIncorrecto'];
+      }
       telefono(blank: false, matches:"[0-9]{3}-[0-9]{6}")
       email (nullable: true, email:true)
     }
